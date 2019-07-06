4e7310c3b687537b17d416e3d6eb4cccc1b0350d
zhoulong <289570126@qq.com>
2019-07-06 17:53:45 +0800

    code pipeline

---
+++ CodePipeline.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+npm install
+
+echo "=== auth ssh "
+
+mkdir ~/.ssh/
+mv key ~/.ssh/id_rsa
+echo "StrictHostKeyChecking no" > ~/.ssh/config
+chmod 400 ~/.ssh/id_rsa
+ssh -T git@github.com
+
+echo "=== git config"
+
+git config --global user.email "longzl@longzl.com"
+git config --global user.name "longzl"
+
+echo "=== hexo install"
+set -x;
+npm install;
+
+npm install hexo-deployer-git --save;
+npm install hexo-filter-plantuml --save;
+npm install hexo-generator-searchdb --save;
+
+cat package.json
+
+echo "=== hexo deploy"
+
+node_modules/hexo/bin/hexo g -d;
