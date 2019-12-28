e8b4bf4693f019b399d136d32c0806dca57471ec
zhoulong <zhoul@bsfit.com.cn>
2019-12-29 04:06:44 +0800

    update

--- Pipeline_build.sh
+++ Pipeline_build.sh
@@ -5,21 +5,10 @@ set -x;
 cnpm install;
 cnpm install hexo-filter-plantuml --save;
 cnpm install hexo-generator-searchdb --save;
-node_modules/hexo/bin/hexo g -d;
 
-echo "=== 移动文件夹"
-rm -rf scaffolds
-rm -rf source
-rm -rf themes
-rm -rf .gitignore
-rm -rf _config.yml
-rm -rf CodePipeline.sh
-rm -rf package.json
-rm -rf Pipeline_build.sh
-rm -rf push2git.sh
-rm -rf README.md
-rm -rf run_service.sh
-mv public/* ./
-rm -rf public
-
+echo "=== 生成静态文件"
+node_modules/hexo/bin/hexo g;
 ls -l
+
+echo "=== 部署到 git"
+node_modules/hexo/bin/hexo d;
